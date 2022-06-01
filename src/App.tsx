42c74aeeefe5827995e44f81f2f5b05ed9bf75d2
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-06-01 17:19:37 +0300

    setting up token

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import {Col, Row} from 'antd';
 import React from 'react';
-import githubLogo from '../src/assets/github-logo.png'
 import "../src/App.scss"
 import 'antd/dist/antd.css';
 import axios from "axios";
-import Searcher from "./UI/components/Searcher/Searcher";
 import {HomeRoute} from "./UI/routes/Routes";
 
 function App() {
     axios.interceptors.request.use((config) => {
         config.baseURL = "https://api.github.com/";
         // @ts-ignore
-        config.headers['Authorization'] = `token ${process.env.REACT_APP_ACCESS_TOKEN}`
+        config.headers = {Authorization: `token ${process.env.REACT_APP_ACCESS_TOKEN}`};
         return config;
     });
     return (
